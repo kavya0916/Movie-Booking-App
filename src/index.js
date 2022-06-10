37c9d713e7cdc0597fa35f65ab6f3ba24e4c4215
Kavya hb <kavyahb21@gmail.com>
2022-06-11 03:28:59 +0530

    Checkpoint 5

--- src/index.js
+++ src/index.js
@@ -1,12 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
 import './index.css';
 import Home from './screens/home/Home';
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+
+    <BrowserRouter>
     <Home />
+    </BrowserRouter>
   </React.StrictMode>,
 
   document.getElementById('root')
